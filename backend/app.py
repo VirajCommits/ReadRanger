91986ac62574e4d8bb18f99c9295593043c293e7
VirajCommits <virajmurab567@yahoo.com>
2024-12-30 16:43:34 -0700

    shifted to heroku , fixing issues

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,5 @@ def index():
 @app.route('/<path:filename>')
 def static_files(filename):
     return send_from_directory(app.static_folder, filename)
+if __name__ == '__main__':
+    app.run(debug=True)  # Set debug=True for development
